2221e6cc6339d870817786c3ba6e32c5df9cc247
chessmanx <bodindet.ro.65@ubu.ac.th>
2025-09-30 11:17:19 +0700

    render ready

--- app.py
+++ app.py
@@ -193,4 +193,8 @@ def technology():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
+    # app.run(host="127.0.0.1", port=port, debug=True)
+
